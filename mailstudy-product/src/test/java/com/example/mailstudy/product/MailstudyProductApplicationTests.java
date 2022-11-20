925ed912e0bba66a7c71a1a58d5d0c7b38ee481b
zhu <ZGY16655827571@outlook.com>
2022-11-20 18:25:09 +0800

    使用人人开源项目，同时使用代码生成器进行逆向生成

--- mailstudy-product/src/test/java/com/example/mailstudy/product/MailstudyProductApplicationTests.java
+++ mailstudy-product/src/test/java/com/example/mailstudy/product/MailstudyProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.mailstudy.product;
 
+import com.example.mailstudy.product.entity.BrandEntity;
+import com.example.mailstudy.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MailstudyProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
+
     }
 
 }
